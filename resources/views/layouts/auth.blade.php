54349d0c97715aac59bad4bbf00afe3d3289f4ed
rasyid <rasyid@engx1.com>
2026-01-06 17:05:04 +0700

    feat: Add favicon link to authentication layout for improved branding

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/assets/home/logo.png') }}" type="image/x-icon">
     <title>@yield('title', 'AturDOit - Solusi Keuangan Digital Anda')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
